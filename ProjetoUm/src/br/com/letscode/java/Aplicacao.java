49ee5ac983c92e9c63bb68ff3e006b9ccb010a09
dante <dsa17@inf.ufpr.br>
2021-10-02 16:04:16 -0300

    adicionado classe biblioteca

--- ProjetoUm/src/br/com/letscode/java/Aplicacao.java
+++ ProjetoUm/src/br/com/letscode/java/Aplicacao.java
@@ -1,8 +1,14 @@
 package br.com.letscode.java;
+import br.com.letscode.java.biblioteca.*;
+
 
 public class Aplicacao {
 
     public static void main(String[] args) {
 	// write your code here
+        Biblioteca bib = new Biblioteca();
+       // bib.CadastrarLivro
+
+
     }
 }
--- ProjetoUm/src/br/com/letscode/java/biblioteca/Biblioteca.java
+++ ProjetoUm/src/br/com/letscode/java/biblioteca/Biblioteca.java
@@ -1,4 +1,62 @@
 package br.com.letscode.java.biblioteca;
+import br.com.letscode.java.biblioteca.livros.Livros;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Biblioteca {
+
+public class Biblioteca<Livros> {
+    private String nome;
+    private String local;
+    private List<Livros> livro;
+
+    public Biblioteca(){}
+
+    public Biblioteca(String nome, String local) {
+        this.nome = nome;
+        this.local = local;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public List<Livros> getLivros() {
+        return livro;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public void setLivros(List<Livros> livro) {
+        this.livro = livro;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Biblioteca{" +
+                "nome='" + nome + '\'' +
+                ", local='" + local + '\'' +
+                '}';
+    }
+
+    public void CadastrarLivro(String isbn, String titulo, String autor, String editora){
+        Livros  livroNovo = new Livros();
+        this.livro.add(livroNovo);
+    }
+
+    public void EmprestarLivro( Pessoa pessoa, Livros livro, int qtd){
+
+
+    }
+
+
 }
