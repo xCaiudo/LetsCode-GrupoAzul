414fd3ee72cbb3002cbac021d49c2a7bd6ce8002
ArthurVillela <arthurvillela@hotmail.com>
2021-10-02 15:32:28 -0300

    created Livro and Enum EstadoLivro

--- ProjetoUm/src/br/com/letscode/java/biblioteca/livros/EstadoLivro.java
+++ ProjetoUm/src/br/com/letscode/java/biblioteca/livros/EstadoLivro.java
@@ -1,4 +1,10 @@
 package br.com.letscode.java.biblioteca.livros;
 
 public enum EstadoLivro {
+    
+    DISPONIVEL ("Disponível"), 
+    ALUGADO ("Alugado");
+
+    EstadoLivro(String alugado) {
+    }
 }
--- ProjetoUm/src/br/com/letscode/java/biblioteca/livros/Livros.java
+++ ProjetoUm/src/br/com/letscode/java/biblioteca/livros/Livros.java
@@ -1,4 +1,71 @@
 package br.com.letscode.java.biblioteca.livros;
 
 public class Livros {
+
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private EstadoLivro estado = EstadoLivro.DISPONIVEL;
+
+    public Livros() {
+    }
+
+    public Livros(String isbn, String titulo, String autor, String editora) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public EstadoLivro getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoLivro estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "isbn='" + isbn + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", editora='" + editora + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
 }
