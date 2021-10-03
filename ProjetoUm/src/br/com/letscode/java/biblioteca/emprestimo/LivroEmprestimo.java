5aa58fbc8a257a7fb31e6c4c63038b34ecd6e51e
Caio <xCaiudo16@gmail.com>
2021-10-02 21:25:14 -0300

    Começando LivroEmprestimo

--- ProjetoUm/src/br/com/letscode/java/biblioteca/emprestimo/LivroEmprestimo.java
+++ ProjetoUm/src/br/com/letscode/java/biblioteca/emprestimo/LivroEmprestimo.java
@@ -1,12 +1,16 @@
 package br.com.letscode.java.biblioteca.emprestimo;
+import br.com.letscode.java.biblioteca.usuario.Aluno;
+import br.com.letscode.java.biblioteca.usuario.Professor;
 import br.com.letscode.java.biblioteca.usuario.Usuario;
 import br.com.letscode.java.biblioteca.livros.Livros;
 import java.time.LocalDate;
 
 public class LivroEmprestimo {
     private LocalDate dataEmprestimo;
+    private LocalDate dataParaDevolucao;
     private Usuario user;
     private Livros liv;
+    private int diasUteis;
 
     public LivroEmprestimo(){}
 
@@ -14,7 +18,18 @@ public class LivroEmprestimo {
         this.dataEmprestimo = dataEmprestimo;
         this.user = user;
         this.liv = liv;
+
+        if (this.user instanceof Aluno){
+            this.diasUteis= 10;
+        }
+        else if(this.user instanceof Professor){
+            this.diasUteis = 20;
+        }
+//        this.dataParaDevolucao = CalculaDataEvolucao(this.diasUteis, this.dataEmprestimo);
     }
+
+//    private LocalDate CalculaDataEvolucao(int diasUteis, LocalDate dataEmprestimo) {
+//    }
 
     public LocalDate getDataEmprestimo() {
         return dataEmprestimo;
